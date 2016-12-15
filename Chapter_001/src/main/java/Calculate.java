b4d66db3427959fd6348f64f061cf56f1b7bc58b
giacint123 <giacint003@gmail.com>
2016-12-15 15:12:29 +0300

    Update Calculate.java

--- Chapter_001/src/main/java/Calculate.java
+++ Chapter_001/src/main/java/Calculate.java
@@ -1,7 +1,13 @@
 package ru.job4j;
 
+/**
+*Class Calculate для вывода на экран информации
+*@author zhigulin
+*@since 15.12.2016
+*/
+
 public class Calculate {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		System.out.println("Hello world.");
 	}
